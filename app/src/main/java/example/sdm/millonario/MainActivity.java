c632a4688005c5007567030ab3f0d2a5c4fa0258
jorllilli8 <36412312+jorllilli8@users.noreply.github.com>
2018-02-26 15:33:54 +0100

    scores & settings

--- app/src/main/java/example/sdm/millonario/MainActivity.java
+++ app/src/main/java/example/sdm/millonario/MainActivity.java
@@ -1,7 +1,11 @@
 package example.sdm.millonario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //cambiooo
 
+    }
+
+    public void btnSettings(View v){
+        Intent i=new Intent(this,SettingsActivity.class);
+        startActivity(i);
+    }
+
+    public void btnScores(View view){
+        Intent i = new Intent(this,ScoresActivity.class);
+        startActivity(i);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       if(item.getItemId() == R.id.home) {
+           return super.onOptionsItemSelected(item);
+       }else{
+           Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+           return true;
+       }
     }
 }
---
+++ app/src/main/java/example/sdm/millonario/ScoresActivity.java
@@ -0,0 +1,22 @@
+package example.sdm.millonario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_millonario,menu);
+        return true;
+    }
+}
